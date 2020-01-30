e5a45cf0a313987bf594fb58bd542eef2572bdb8
rockuo <richardbures@email.cz>
2020-01-30 19:20:42 +0100

    Jídlo JUR

--- recipes.js
+++ recipes.js
@@ -1,96 +1,119 @@
 export default [
-	{
-		name: 'Jablečný štrůdl / Apple pie',
-		url: 'https://dev-breakfast.webnode.com/l/apple-pie/',
-		recipe: [
-			'puff pastry',
-			'apples',
-			'cinnamon',
-			'sugar',
-			'raisins',
-			'walnuts',
-			'eggs',
-			'sunflower seeds',
+    {
+        name: 'Jablečný štrůdl / Apple pie',
+        url: 'https://dev-breakfast.webnode.com/l/apple-pie/',
+        recipe: [
+            'puff pastry',
+            'apples',
+            'cinnamon',
+            'sugar',
+            'raisins',
+            'walnuts',
+            'eggs',
+            'sunflower seeds',
 
-		],
-	},
-	{
-		name: 'Curd cheese pie',
-		url: 'https://dev-breakfast.webnode.com/l/curd-cheese-pie/',
-		recipe: [
-			'puff pastry',
-			'curd cheese',
-			'sunflower seeds',
-			'sugar',
-			'raisins',
-			'walnuts',
-			'eggs',
+        ],
+    },
+    {
+        name: 'Curd cheese pie',
+        url: 'https://dev-breakfast.webnode.com/l/curd-cheese-pie/',
+        recipe: [
+            'puff pastry',
+            'curd cheese',
+            'sunflower seeds',
+            'sugar',
+            'raisins',
+            'walnuts',
+            'eggs',
 
-		],
-	},
-	{
-		name: 'Ham snails',
-		url: 'https://dev-breakfast.webnode.com/l/ham-snails/',
-		recipe: [
-			'ham',
-			'salami',
-			'cheese',
-			'ketchup',
-			'puff pastry',
-			'eggs',
+        ],
+    },
+    {
+        name: 'Ham snails',
+        url: 'https://dev-breakfast.webnode.com/l/ham-snails/',
+        recipe: [
+            'ham',
+            'salami',
+            'cheese',
+            'ketchup',
+            'puff pastry',
+            'eggs',
 
-		],
-	},
-	{
-		name: 'Quiche',
-		url: 'https://dev-breakfast.webnode.com/l/quiche/',
-		recipe: [
-			'puff pastry',
-			'bacon',
-			'cheese',
-			'eggs',
-			'heavy cream',
-		],
-	},
-	{
-		name: 'Sardine spread',
-		url: 'https://dev-breakfast.webnode.com/l/sardine-spread/',
-		recipe: [
-			'margarine',
-			'cream cheese',
-			'sardne',
-			'red onion',
-			'cucumbers (Znojmo!)',
-			'eggs',
+        ],
+    },
+    {
+        name: 'Quiche',
+        url: 'https://dev-breakfast.webnode.com/l/quiche/',
+        recipe: [
+            'puff pastry',
+            'bacon',
+            'cheese',
+            'eggs',
+            'heavy cream',
+        ],
+    },
+    {
+        name: 'Sardine spread',
+        url: 'https://dev-breakfast.webnode.com/l/sardine-spread/',
+        recipe: [
+            'margarine',
+            'cream cheese',
+            'sardne',
+            'red onion',
+            'cucumbers (Znojmo!)',
+            'eggs',
 
-		],
-	},
-	{
-		name: 'Bábovka with Irish cream',
-		url: 'https://dev-breakfast.webnode.com/l/b-with-irish-cream/',
-		recipe: [
-			'flour',
-			'baking powder (aka prdopeč)',
-			'almond',
-			'chocolate',
-			'butter',
-			'eggs',
-			'sugar',
-			'eggnog (or irish-cream)',
+        ],
+    },
+    {
+        name: 'Bábovka with Irish cream',
+        url: 'https://dev-breakfast.webnode.com/l/b-with-irish-cream/',
+        recipe: [
+            'flour',
+            'baking powder (aka prdopeč)',
+            'almond',
+            'chocolate',
+            'butter',
+            'eggs',
+            'sugar',
+            'eggnog (or irish-cream)',
 
-		],
-	},
-	{
-		name: 'Aubergine spread',
-		url: 'https://dev-breakfast.webnode.com/l/aubergine-spread/',
-		recipe: [
-			'aubergine',
-			'curd',
-			'mayo',
-			'olive oil',
-			'salt',
-			'pepper',
-			'thyme',
-		]
-	},
+        ],
+    },
+    {
+        name: 'Aubergine spread',
+        url: 'https://dev-breakfast.webnode.com/l/aubergine-spread/',
+        recipe: [
+            'aubergine',
+            'curd',
+            'mayo',
+            'olive oil',
+            'salt',
+            'pepper',
+            'thyme',
+        ]
+    },
+    {
+        name: 'Guacamole',
+        url: 'https://dev-breakfast.webnode.com/l/guacamole/',
+        recipe: [
+            'avocado',
+            'tomatoes',
+            'lime',
+            'coriander',
+            'onion',
+            'lemon',
+        ]
+    },
+    {
+        name: 'Hummus',
+        url: 'https://dev-breakfast.webnode.com/l/hummus/',
+        recipe: [
+            'chickpeas',
+            'tahini',
+            'olive oil',
+            'onion',
+        ]
+    },
+
 ];
